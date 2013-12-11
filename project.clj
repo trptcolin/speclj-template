f6ae3a6a6e10f2a2a5034127ae5cd21ffad4c0a9
Micah <micahmartin@gmail.com>
2013-12-11 09:15:24 -0600

    upgrades to speclj 2.9.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject speclj/lein-template "0.1.5"
+(defproject speclj/lein-template "0.1.6"
   :description "A Leiningen template for speclj"
   :url "http://speclj.com"
   :license {:name "Eclipse Public License"
