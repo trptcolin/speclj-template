7b154cc340a0a7899ff1236ab6ffba250cb19307
Micah <micahmartin@gmail.com>
2015-06-19 16:22:37 -0500

    updated for 3.3.0

--- src/leiningen/new/speclj/project.clj
+++ src/leiningen/new/speclj/project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.2.0"]]}}
-  :plugins [[speclj "3.2.0"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.0"]]}}
+  :plugins [[speclj "3.3.0"]]
   :test-paths ["spec"])
