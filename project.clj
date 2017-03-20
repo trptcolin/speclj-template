dd766c916fd7df2ff3589e561cb33d3b9febd563
Colin Jones <trptcolin@gmail.com>
2017-03-20 11:07:59 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject speclj/lein-template "3.3.1"
+(defproject speclj/lein-template "3.3.2"
   :description "A Leiningen template for speclj"
   :url "http://speclj.com"
   :license {:name "Eclipse Public License"
