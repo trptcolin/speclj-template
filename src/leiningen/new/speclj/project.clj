90901f67bb3ba7ff139ea3bb9373506acbb7843e
Jason <jgoodman@8thlight.com>
2017-03-20 11:00:59 -0500

    Update speclj and clojure versions

--- src/leiningen/new/speclj/project.clj
+++ src/leiningen/new/speclj/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]]
-  :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
-  :plugins [[speclj "3.3.1"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
